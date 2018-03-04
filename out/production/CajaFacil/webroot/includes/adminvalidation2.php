700be9f08c940d516985316a33da703c09d4fe8e
Luis Sencion <luisen14@gmail.com>
2018-03-03 21:02:31 -0600

    removed out folder

--- out/production/CajaFacil/webroot/includes/adminvalidation2.php
+++
@@ -1,8 +0,0 @@
-<?
-//si el usuario tiene permiso para la pantalla, si no avisa y va a la inicial
-if (trim(permiso($pPantalla))=="")
-{
-	aviso("No tiene permiso para ingresar a esta pantalla. Debe solicitarlo al administrador del sistema."); 
-	echo "<script>window.close();</script>";
-}
-?>
